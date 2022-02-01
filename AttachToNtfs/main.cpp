4522d210d6527dd7d8bd1982c8455035b2942043
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-02-01 20:37:08 +0800

    update

--- AttachToNtfs/main.cpp
+++ AttachToNtfs/main.cpp
@@ -16,7 +16,9 @@ AttachToNtfs!_DEVICE_OBJECT .LowerDevices
 挂载后的设备栈 AttachToNtfs.sys -> FltMgr.sys(minifilter) -> Ntfs  (最左边的最先收到IRP)
 
 
-
+	UpperDevices     : Immediately above is Device for "\Driver\AttachToNtfs" [at 0xffffa38f0c33a9f0]
+	LowerDevices     : Immediately below is Device for "\FileSystem\Ntfs" [at 0xffffa38f09603030]
+	Driver           : 0xffffa38f0957bd20 : Driver "\FileSystem\FltMgr" [Type: _DRIVER_OBJECT *]
 
 
 
@@ -31,11 +33,15 @@ AttachToNtfs!_DEVICE_OBJECT .LowerDevices
 
 */
 
-void DriverUnload(PDRIVER_OBJECT DriverObject);
+
 
 UNICODE_STRING AnonymousNtfs = RTL_CONSTANT_STRING(L"\\FileSystem\\AnonymousNtfs");
 UNICODE_STRING Ntfs = RTL_CONSTANT_STRING(L"\\??\\C:\\Windows\\System32\\drivers\\ntfs.sys");
 UNICODE_STRING NtfsDeviceName = RTL_CONSTANT_STRING(L"\\FileSystem\\Ntfs");
+
+
+UNICODE_STRING Npfs = RTL_CONSTANT_STRING(L"\\??\\C:\\Windows\\System32\\drivers\\npfs.sys");
+UNICODE_STRING NpfsDeviceName = RTL_CONSTANT_STRING(L"\\FileSystem\\Npfs");
 
 FILE_OBJECT* NtfsFile;
 DEVICE_OBJECT* NtfsDevice;
@@ -53,7 +59,43 @@ if (!NT_SUCCESS(Status))    \
 	return Status;\
 }
 
+void DriverUnload(PDRIVER_OBJECT DriverObject);
 NTSTATUS ATPassThrouth(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS ATHandler_IRP_CREATE(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
+bool FindSubUnicodeString(const WCHAR * sub, PCUNICODE_STRING src);
+
+
+
+bool FindSubUnicodeString(const WCHAR * sub, PCUNICODE_STRING src)
+{
+	if (!sub || !src || !src->Buffer)
+		return false;
+
+	WCHAR* tmp = (WCHAR*)ExAllocatePoolWithTag(NonPagedPool, src->Length + sizeof(L'\0'), 'bbs');
+	WCHAR* sub_t = (WCHAR*)ExAllocatePoolWithTag(NonPagedPool, wcslen(sub) * 2 + sizeof(L'\0'), 'bbs');
+
+	if (!tmp || !sub_t)
+		return false;
+
+	memcpy(tmp, src->Buffer, src->Length);
+	tmp[src->Length / 2] = L'\0';
+
+
+	memcpy(sub_t, sub, wcslen(sub) * 2);
+
+	sub_t[wcslen(sub)] = L'\0';
+
+	//RtlDowncaseUnicodeArray(tmp);
+	//RtlDowncaseUnicodeArray(sub_t);
+
+	bool ret = (wcsstr(tmp, sub_t)) ? true : false;
+
+	ExFreePoolWithTag(tmp, 'bbs');
+	ExFreePoolWithTag(sub_t, 'bbs');
+
+
+	return ret;
+}
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegPath)
 {
@@ -61,6 +103,7 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Reg
 
 	DriverObject->DriverUnload = DriverUnload;
 
+#if 1 //挂载ntfs
 	Status = IoGetDeviceObjectPointer(&Ntfs, FILE_ALL_ACCESS, &NtfsFile, &NtfsDevice);
 
 	COMMON_FAILED(Status, "IoGetDeviceObjectPointer");
@@ -75,6 +118,24 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Reg
 		DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_ERROR_LEVEL, "IoAttachDevice failed with %x\n", Status);
 		return Status;
 	}
+#endif
+
+#if 0 //挂载npfs
+	Status = IoGetDeviceObjectPointer(&Npfs, FILE_ALL_ACCESS, &NtfsFile, &NtfsDevice);
+
+	COMMON_FAILED(Status, "IoGetDeviceObjectPointer");
+
+	Status = IoCreateDevice(DriverObject, 0, &AnonymousNtfs, NtfsDevice->DeviceType, 0, false, &NewDevice);
+	COMMON_FAILED(Status, "IoCreateDevice");
+
+	Status = IoAttachDevice(NewDevice, &Npfs, &TargetDevice);
+	if (!NT_SUCCESS(Status))
+	{
+		IoDeleteDevice(NewDevice);
+		DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_ERROR_LEVEL, "IoAttachDevice failed with %x\n", Status);
+		return Status;
+	}
+#endif
 	
 	Ptr_FAST_IO_DISPATCH = (PFAST_IO_DISPATCH)ExAllocatePoolWithTag(NonPagedPoolNx, sizeof(FAST_IO_DISPATCH), 'ifMF');
 	
@@ -125,8 +186,9 @@ extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Reg
 		DriverObject->MajorFunction[i] = ATPassThrouth;
 
 	
+	// 过滤irp mj create
 
-
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = ATHandler_IRP_CREATE;
 
 
 
@@ -192,11 +254,51 @@ void DriverUnload(PDRIVER_OBJECT DriverObject)
 */
 NTSTATUS ATPassThrouth(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
+
+#if 0		//将irp转发给minifilter
+	IoSkipCurrentIrpStackLocation(Irp);
+	return IofCallDriver(DeviceObject->DeviceObjectExtension->AttachedTo, Irp);
+#endif
+
+
+#if 1       //将irp直接转发给ntfs
+
+	IoSkipCurrentIrpStackLocation(Irp);
+	if (DeviceObject->DeviceObjectExtension->AttachedTo->DeviceObjectExtension->AttachedTo) {
+		return IofCallDriver(DeviceObject->DeviceObjectExtension->AttachedTo->DeviceObjectExtension->AttachedTo, Irp);
+	}
+	else {
+		return IofCallDriver(DeviceObject->DeviceObjectExtension->AttachedTo, Irp);
+	}
+#endif
+
+
+
+}
+
+NTSTATUS ATHandler_IRP_CREATE(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
+{
+
+	ASSERT(Irp->Tail.Overlay.CurrentStackLocation->MajorFunction == IRP_MJ_CREATE);
+
+	FILE_OBJECT* FileObject = Irp->Tail.Overlay.CurrentStackLocation->FileObject;
+
+	//DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_ERROR_LEVEL, "[IRP Create] %wZ\n", FileObject->FileName);
+
+	//
+	//过滤irp
+	//
+	if (FindSubUnicodeString(L"ok.txt", &FileObject->FileName))
+	{
+		IofCompleteRequest(Irp, IO_NO_INCREMENT);
+		Irp->IoStatus.Status = STATUS_ACCESS_DENIED;
+		return STATUS_SUCCESS;
+	}
+
+
 	IoSkipCurrentIrpStackLocation(Irp);
 	return IofCallDriver(DeviceObject->DeviceObjectExtension->AttachedTo, Irp);
 }
-
-
 
 
 
--- readme.md
+++ readme.md
@@ -3,4 +3,6 @@
 
 将驱动挂载到ntfs上,过滤irp
 
+绕过minifilter等文件过滤
+
 
